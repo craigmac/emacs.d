4b0c3b141173fc68bbdc1b8b30e7946539d7f1c5
Craig MacEachern <craigm@fastmail.com>
2021-01-27 12:08:24 -0500

    compilation and grep mode configured to taste

--- init.el
+++ init.el
@@ -12,20 +12,17 @@
 (setq package-selected-packages
       '(ace-window
 	avy
-	counsel
 	company
 	eglot
 	modus-operandi-theme
 	project
-	ivy
-	swiper
 	which-key))
 (package-install-selected-packages)
 
 (global-set-key (kbd "M-o") 'ace-window)
 (global-set-key (kbd "C-;") 'avy-goto-char)
-(global-set-key (kbd "M-n") 'flycheck-next-error)
-(global-set-key (kbd "M-p") 'flycheck-previous-error)
+(define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
+(define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error)
 
 (load-theme 'modus-operandi t)
 
@@ -33,25 +30,28 @@
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 2)
 
-(counsel-mode)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
-(global-set-key (kbd "C-x 4 b") 'counsel-switch-buffer-other-window)
-(global-set-key (kbd "C-`") 'counsel-switch-to-shell-buffer)
-;; find files limited to git tracked ones
-(global-set-key (kbd "C-c g") 'counsel-git)
-;; live git grep of files
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-;; same but using ag to find anything not-git controlled
-(global-set-key (kbd "C-c k") 'counsel-ag)
-
-(ivy-mode)
-(setq ivy-use-virtual-buffers t)
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "C-r") 'swiper)
-
 (which-key-mode)
+
+;; Ido
+;; ==============================
+;; M-n/p cycles through previous directories from history of searches
+;; M-k to remove current directory from the history
+;; C-f to escape anything ido and enter normal 'find-file'
+;; C-t toggle regexp search within ido prompt
+;; C-b|C-t to switch between find file/buffer.
+;; The head of buffer or file list can be killed with C-k, C-S-b buries
+;; the buffer at the end of the list.
+;;
+;; TODO: add more ignored dirs: elpa/ semanticdb/ eshell/ auto-save-list/ .git/
+;; move all files matching "Summary" or "output\*$" to the end of the list
+;; TODO: add to the list of ignores!
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+(add-hook 'ido-make-buffer-list-hook 'ido-summary-buffers-to-end)
+(setq ido-use-filename-at-point 'guess)	; best guess if file or URL
+(setq ido-ignore-extensions t)
+(setq ido-cannot-complete-command 'ido-prev-match) ; don't popup help buffer
 
 ;; INTERNAL VARIABLES
 (defconst private-dir (expand-file-name "private" user-emacs-directory))
@@ -61,6 +61,7 @@
       save-interprogram-paste-before-kill t
       inhibit-startup-message t
       inhibit-splash-screen t
+      initial-scratch-message nil
       history-length 1000
       visible-bell nil
       create-lockfiles nil
@@ -79,29 +80,70 @@
 (when (equal system-type 'windows-nt)
   (setq-default w32-pipe-read-delay 0)
   (setenv "PATH"
-    (concat "C:\\gnuwin32\\bin" path-separator (getenv "PATH")))
-  ;; github.com/magit/with-editor/issues/41 solution for Windows
-  (defadvice server-ensure-safe-dir
-      (around my-around-server-ensure-safe-dir activate)
-    "Ignores any errors raised from server-ensure-safe-dir"
-    (ignore-errors ad-do-it)))
+    (concat "C:\\gnuwin32\\bin" path-separator (getenv "PATH"))))
+
 (when (eq system-type 'darwin)
-  ;; add homebrew bin
   (setenv "PATH"
 	  (concat "/usr/local/bin:" (getenv "PATH")))
+  ;; for GNU grep/find/locate and few others. Requires
+  ;; brew install coreutils findutils grep
+  (add-to-list 'exec-path "/usr/local/opt/grep/libexec/gnubin")
   (add-to-list 'exec-path "/usr/local/bin")
   (setq mac-option-modifier 'meta)
   (setq mac-command-modifier 'control)
   (setq mac-control-modifier 'control)
   (setq mac-right-option-modifier 'none))
-(when (equal system-type 'gnu/linux))
 
 ;; KEYBINDINGS
 (global-set-key (kbd "C-+") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-z") 'zap-up-to-char)
 (global-set-key "\C-o" 'occur)
-(global-set-key (kbd "<f6>") 'compile)
+
+;; COMPILATION / GREP
+;; TIP: control env passed to compilation command with
+;; variable 'compilation-environment'
+;; TIP: usually we want 'grep-find' not just 'grep'; former reads subdirs
+;; recursively, so whole tree of files from current location. 'grep' is
+;; defaults to current directory.
+(global-set-key (kbd "<f5>") 'compile)
+(global-set-key (kbd "C-<f5>") 'kill-compilation)
+(global-set-key (kbd "<f6>") 'recompile)
+
+;; TODO: get these into an eval-after-load or something?
+(require 'compile)
+(setq compilation-scroll-output t)
+(setq compilation-always-kill t)
+(setq compilation-auto-jump-to-first-error t)
+;; set next/previous result/error keys (M-g M-n/p) to look for buffers with
+;; error/matches in windows of current frame only, and then for a buffer
+;; used previously by 'next-error' or 'previous-error'
+(setq next-error-find-buffer-function 'next-error-buffer-on-selected-frame)
+;; 1 means skip anything less important then a warning,
+;; 2 means skip anything less important than an error, and 0 dont' skip any.
+(setq compilation-skip-threshold 0)
+;; turn on auto display of corresponding source locations of error/matches
+;; when moving through the list of them in a grep/compilation output buffer
+
+;; TODO: add elpa/ and other directories to be ignored when doing 'grep-find'
+;; or 'rgrep' (more interactive version of grep-find)
+
+;; TODO: buffer only so I need to toggle this only in compilation/grep buffers
+;;(next-error-follow-minor-mode)
+
+;; NOTE: needs GNU grep, see how this is done in 'darwin'
+;; os detection below, might have to configure 'grep-apply-setting' directly
+;; instead of letting grep-compute-defaults try to figure it out, but that
+;; should be last resort where emacs cannot find our GNU grep for some reason.
+;; (setq grep-highlight-matches t)
+(require 'grep)
+(setq grep-save-buffers t)		; Don't ask: save all buffers then run
+
+;; TODO: 'grep-find-ignored-directories' pulls its default from
+;; 'vc-directory-exclusion-list' so let's add our junk to that list to benefit
+;; both:
+(add-to-list 'vc-directory-exclusion-list "elpa")
+
 (global-set-key "%" 'match-paren)
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 (global-set-key (kbd "M-i") 'imenu)
@@ -109,6 +151,11 @@
 (global-unset-key (kbd "M-SPC"))
 (global-set-key (kbd "M-SPC w") 'save-buffer)
 (global-set-key (kbd "M-SPC b") 'ibuffer)
+(global-set-key (kbd "M-SPC r") 'grep-find)
+
+;; TODO: bind 'find-grep-dired' as well
+
+;; TODO: make deletion hungrier globally, I like it.
 
 ;; FUNCTIONS
 (defun compile-and-run ()
@@ -149,12 +196,7 @@
 
 ;; UI
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:family "Triplicate T4c" :foundry "outline" :slant normal :weight normal :height 180 :width normal)))))
-(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
@@ -172,18 +214,12 @@
 (transient-mark-mode)
 (auto-image-file-mode)
 (semantic-mode)
-
-
-(setq ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(ido-mode 1)
-(setq ido-use-filename-at-point 'guess)
-(setq ido-create-new-buffer 'always)
+;; in only modes stemming from prog-mode
+(add-hook 'prog-mode-hook (flymake-mode))
+;; flymake configuration, log-level value must be lower or equal to
+;; warning-minimum-level because things not logged will not be displayed
+;; in buffer either.
+(setq warning-minimum-level :error)	  ; popup shows only on 'error' level
+(setq warning-minimum-log-level :warning) ; log these to *Flymake Log*
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ace-window)))
